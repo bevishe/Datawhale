f8a1d665b42f44d4ad7d0e20b2227aada114f49b
BevisHe <hepengme@163.com>
2019-03-04 20:58:47 +0800

    day4

--- crawl/code/crawlMovie/Getdata/xpath.py
+++ crawl/code/crawlMovie/Getdata/xpath.py
@@ -1,20 +1,33 @@
-import  requests
+import requests
 from lxml import etree
-url='http://www.dxy.cn/bbs/thread/626626#626626'
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko'}
-x= requests.get(url, headers=headers, timeout=3)
-x.encoding=x.apparent_encoding
-wb_data=x.text
-html = etree.HTML(wb_data)
-data=[]
-new_data=[]
-result_txt = html.xpath('//td[@class="postbody"]/text()')#找到所需要的数据
-print(result_txt)
-result_name=html.xpath('//div[@class="auth"]/a/text()')
-print(result_name)
-for i in range(0,4):
-    data.append(result_name[i]+"###"+result_txt[i])
-for i in data:
-    x=i.replace("\n","").replace("\t","").replace(" ","")
-    new_data.append(x)
-print(new_data)
+from requests.packages.urllib3.exceptions import RequestError
+
+# 获得要爬取的页面
+def get_one_page(url):
+    try:
+        response = requests.get(url = url)
+        if response.status_code == 200:
+            return response
+        else:
+            print("接收页面失败")
+    except RequestError:
+        print(RequestError)
+
+def get_one_html_lxml(html):
+    html = etree.HTML(html)
+    big = html.xpath('//*[@id="postcontainer"]')
+    for _ in big:
+        # 获取用户名
+        print(_.xpath('div/table/tbody/tr/td[1]/div[2]/a/text()'))
+        # 获取发表的内容
+        print(_.xpath('div/table/tbody/tr/td[2]/div[2]/div[2]/table/tbody/tr/td/text()'))
+    # name = big.xpath('div/table/tbody/tr/td[1]/div[2]/a/text()')
+    # content = big.xpath('div/table/tbody/tr/td[2]/div[2]/div[2]/table/tbody/tr/td/text()')
+    # for i in range(0,len(big.xpath('div'))):
+    #     print(name[i])
+    #     print(content[i])
+
+
+if __name__ == '__main__':
+    html = get_one_page('http://www.dxy.cn/bbs/thread/626626#626626').text
+    get_one_html_lxml(html)
